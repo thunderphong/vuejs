816bc3e13e8814027da31a5a78120cdbf067bbfb
thunderphong <thunderthunderphong@gmail.com>
2020-09-14 22:10:05 +0700

    finally day 5

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <h1> aaaa </h1>
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <header> <h1>Server Status</h1> </header>
+        </div>
+    </div>
+    <hr>        
+    <Home></Home>
+    <hr>
+    <div class="row">
+        <div class="col-xs-12">
+            <footer> <p>All Servers are managed here</p> </footer>
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// }
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
