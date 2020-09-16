f8f0a88bc492975b80911b748656fcd699c93ea8
thunderphong <thunderthunderphong@gmail.com>
2020-09-16 21:34:05 +0700

    day 6

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,33 +1,11 @@
 <template>
-  <div class="container">
-    <div class="row">
-        <div class="col-xs-12">
-            <header> <h1>Server Status</h1> </header>
-        </div>
-    </div>
-    <hr>        
-    <Home></Home>
-    <hr>
-    <div class="row">
-        <div class="col-xs-12">
-            <footer> <p>All Servers are managed here</p> </footer>
-        </div>
-    </div>
-  </div>
+
 </template>
 
 <script>
-import Home from './components/Home.vue'
-
-export default {
-  name: 'App',
-  components: {
-    Home
-  }
-}
-
 
 </script>
 
 <style>
+
 </style>
---
+++ test/src/components/UserEdit.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="component">
+        <h3>You may edit the User here</h3>
+        <p> Age: {{ age }} </p>
+        <button @click="editAge"> Change Age </button>
+        <p>Edit me!</p>
+    </div>
+</template>
+
+<script>
+    import {eventBus} from '../main';
+
+    export default {
+        props: {
+            age: Number
+        },
+        methods: {
+            editAge(){
+                this.age = 90;
+                eventBus.$emit('ageWasEdited', this.age)
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+    div {
+        background-color: lightgreen;
+    }
+</style>
