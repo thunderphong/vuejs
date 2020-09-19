db2d1726fae3d2c4cb987ba8244c7e25080735c6
thunderphong <thunderthunderphong@gmail.com>
2020-09-19 20:46:12 +0700

    day 7

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,11 +1,45 @@
 <template>
-
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New</button>
+                <hr />
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                        <h2 slot="title">{{ quoteTitle }}</h2>
+                        <p>A wonderful quote</p>
+                    </component>
+                </keep-alive>
+                <!-- <appQuote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A wonderful quote</p>
+                </appQuote> -->
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import Quote from "./components/Quote";
+import New from "./components/New";
+import Author from "./components/Author";
 
+export default {
+    data: function() {
+        return {
+            quoteTitle: "aasdasd Quote",
+            selectedComponent: "appQuote",
+        };
+    },
+    components: {
+        appQuote: Quote,
+        appAuthor: Author,
+        appNew: New,
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ test/src/components/New.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h3>Something new</h3>
+    </div>  
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+</style>
